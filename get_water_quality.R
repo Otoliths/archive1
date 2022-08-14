1b5b5d70c48e7dfe32c9898875d1c12d9a729a05
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2022-08-14 22:25:00 +0800

    Update get_water_quality.R

--- get_water_quality.R
+++ get_water_quality.R
@@ -1,40 +1,7 @@
+options(timeout=1000)
 if (!file.exists(paste0("water_quality_data/",as.POSIXlt(Sys.Date(), "Asia/Shanghai")))){
   dir.create(paste0("water_quality_data/",as.POSIXlt(Sys.Date(), "Asia/Shanghai")))
 }
 path <- paste0("water_quality_data/",as.POSIXlt(Sys.Date(), "Asia/Shanghai"),"/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".json")
 
-download.file("http://106.37.208.244:10001/Home/GetSectionDataList?&page=1&rows=9999",path)
-#system(paste('wget','http://106.37.208.244:10001/Home/GetSectionDataList?&page=1&rows=9999','-O',path))
-#http://106.37.208.244:10001/Home/GetSectionDataList?taskMonth=202101&page=1&rows=9999
-
-# if (!requireNamespace(c("jsonlite","rlist","httr"), quietly = TRUE))
-#   install.packages("jsonlite","rlist","httr",dependencies = T)
-
-# url <- "http://106.37.208.243:8068/GJZ/Ajax/Publish.ashx?AreaID=&RiverID=&MNName=&PageIndex=1&PageSize=9999&action=getRealDatas"
-# re1 <- jsonlite::fromJSON(url, simplifyVector = FALSE)
-# re1$tbody <- as.data.frame(rlist::list.rbind(re1$tbody))
-# re1$tbody <- apply(re1$tbody,1,function(x){gsub("<.*?>", "", x)})
-# re1$tbody <- as.data.frame(t(re1$tbody))
-# names(re1$tbody) <- gsub("<.*?>", "", unlist(re1$thead))
-# re1$thead <- gsub("<.*?>", "", re1$thead)
-
-
-# url <- "http://envi.ckcest.cn/environment/WaterMonitor/index.action"
-# re1 <- jsonlite::fromJSON(url, simplifyVector = FALSE)
-# re1 <- as.data.frame(rlist::list.rbind(re1$data))
-
-
-
-#re2 <- jsonlite::fromJSON("http://106.37.208.244:10001/Home/GetSectionDataList?", simplifyVector = FALSE)
-#re2 <- as.data.frame(rlist::list.rbind(re2))
-#download_date <- rep(Sys.time(),dim(re2)[1])
-#http_date <- rep(httr::http_date(Sys.time()),dim(re2)[1])
-#re2 <- cbind(download_date,http_date,re2)
-
-#res <- list(less = re1,more = re2)
-
-# if (!file.exists(paste0("water_quality_data/",as.POSIXlt(Sys.Date(), "Asia/Shanghai")))){
-#   dir.create(paste0("water_quality_data/",as.POSIXlt(Sys.Date(), "Asia/Shanghai")))
-# }
-# path <- paste0("water_quality_data/",as.POSIXlt(Sys.Date(), "Asia/Shanghai"),"/",as.POSIXlt(Sys.time(), "Asia/Shanghai"),".rds")
-# saveRDS(re1,path)
+try(download.file("http://106.37.208.244:10001/Home/GetSectionDataList?&page=1&rows=9999",path),silent = T)
