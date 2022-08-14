02ef784bcd5aaaf734512552d58c70b2d72a64ff
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2022-08-14 23:33:33 +0800

    Update get_water_quality.R

--- get_water_quality.R
+++ get_water_quality.R
@@ -1,3 +1,31 @@
 path <- paste0("water_quality_data/",Sys.time(),".json")
 
 download.file("http://106.37.208.244:10001/Home/GetSectionDataList?&page=1&rows=9999",path)
+
+baseurl1 <- "http://xxfb.mwr.cn/hydroSearch/"
+baseurl2 <- "http://xxfb.mwr.cn/portal/"
+query1 <- c("greatRiver","greatRsvr")
+query2 <- c("vitalRiverInfo","vitalRsvrInfo","earlyWarningInfo","hydroinfoByDays","surpassWarningInfo")
+
+
+hydroSearch <- lapply(1:length(query1),function(x){
+  jsonlite::fromJSON(paste0(baseurl1,query1[x]))
+})
+
+portal <- lapply(1:length(query2),function(x){
+  jsonlite::fromJSON(paste0(baseurl2,query2[x]))
+})
+
+hydrological <- list(greatRiver = hydroSearch[[1]],
+                     greatRsvr = hydroSearch[[2]],
+                     vitalRiverInfo = portal[[1]],
+                     vitalRsvrInfo = portal[[2]],
+                     earlyWarningInfo = portal[[3]],
+                     hydroinfoByDays = portal[[4]],
+                     surpassWarningInfo = portal[[5]])
+if (!file.exists(paste0("hydrological_data/",as.POSIXlt(Sys.Date(), "Asia/Shanghai")))){
+  dir.create(paste0("hydrological_data/",as.POSIXlt(Sys.Date(), "Asia/Shanghai")))
+}
+path <- paste0("hydrological_data/",Sys.time(), ".rds")
+saveRDS(hydrological,path)
+
